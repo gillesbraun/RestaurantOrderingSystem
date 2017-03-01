edbb18f50c5db65dda2294bb48746608a248d88c
Gilles Braun <braun.gillo@gmail.com>
2017-03-01 13:43:28 +0100

    Add Allergen Controllers

---
+++ server/src/main/java/lu/btsi/bragi/ros/server/controller/AllergenController.java
@@ -0,0 +1,49 @@
+package lu.btsi.bragi.ros.server.controller;
+
+import lu.btsi.bragi.ros.models.pojos.Allergen;
+import lu.btsi.bragi.ros.server.database.tables.records.AllergenRecord;
+
+import java.util.List;
+
+/**
+ * Created by gillesbraun on 01/03/2017.
+ */
+public class AllergenController extends Controller<Allergen> {
+
+    private static Class mapTo = Allergen.class;
+
+    private lu.btsi.bragi.ros.server.database.tables.Allergen dbTable = lu.btsi.bragi.ros.server.database.tables.Allergen.ALLERGEN;
+
+
+    public AllergenController() {
+        super(mapTo);
+    }
+
+    @Override
+    protected List<Allergen> handleGet() throws Exception {
+        List<Allergen> list = context.fetch(dbTable).into(mapTo);
+        return list;
+    }
+
+    @Override
+    protected void handleUpdate(Allergen obj) throws Exception {
+        AllergenRecord allergenRecord = new AllergenRecord();
+        allergenRecord.from(obj);
+        allergenRecord.reset(dbTable.UPDATED_AT);
+        context.executeUpdate(allergenRecord);
+    }
+
+    @Override
+    protected void handleCreate(Allergen obj) throws Exception {
+        AllergenRecord allergenRecord = new AllergenRecord();
+        allergenRecord.from(obj);
+        context.executeInsert(allergenRecord);
+    }
+
+    @Override
+    protected void handleDelete(Allergen obj) throws Exception {
+        AllergenRecord allergenRecord = new AllergenRecord();
+        allergenRecord.from(obj);
+        context.executeDelete(allergenRecord);
+    }
+}
---
+++ server/src/main/java/lu/btsi/bragi/ros/server/controller/AllergenLocalizedController.java
@@ -0,0 +1,49 @@
+package lu.btsi.bragi.ros.server.controller;
+
+import lu.btsi.bragi.ros.models.pojos.AllergenLocalized;
+import lu.btsi.bragi.ros.server.database.tables.records.AllergenLocalizedRecord;
+
+import java.util.List;
+
+/**
+ * Created by gillesbraun on 01/03/2017.
+ */
+public class AllergenLocalizedController extends Controller<AllergenLocalized> {
+
+    private static Class mapTo = AllergenLocalized.class;
+
+    private lu.btsi.bragi.ros.server.database.tables.AllergenLocalized dbTable = lu.btsi.bragi.ros.server.database.tables.AllergenLocalized.ALLERGEN_LOCALIZED;
+
+
+    public AllergenLocalizedController() {
+        super(mapTo);
+    }
+
+    @Override
+    protected List<AllergenLocalized> handleGet() throws Exception {
+        List<AllergenLocalized> list = context.fetch(dbTable).into(mapTo);
+        return list;
+    }
+
+    @Override
+    protected void handleUpdate(AllergenLocalized obj) throws Exception {
+        AllergenLocalizedRecord allergenLocalizedRecord = new AllergenLocalizedRecord();
+        allergenLocalizedRecord.from(obj);
+        allergenLocalizedRecord.reset(dbTable.UPDATED_AT);
+        context.executeUpdate(allergenLocalizedRecord);
+    }
+
+    @Override
+    protected void handleCreate(AllergenLocalized obj) throws Exception {
+        AllergenLocalizedRecord allergenLocalizedRecord = new AllergenLocalizedRecord();
+        allergenLocalizedRecord.from(obj);
+        context.executeInsert(allergenLocalizedRecord);
+    }
+
+    @Override
+    protected void handleDelete(AllergenLocalized obj) throws Exception {
+        AllergenLocalizedRecord allergenLocalizedRecord = new AllergenLocalizedRecord();
+        allergenLocalizedRecord.from(obj);
+        context.executeDelete(allergenLocalizedRecord);
+    }
+}
