c3c158f9fa3f6d427f24a9569d79a715d83ce2bb
Gilles Braun <braun.gillo@gmail.com>
2017-03-28 17:26:06 +0200

    Added Config file

--- server/src/main/java/lu/btsi/bragi/ros/server/DatabaseModule.java
+++ server/src/main/java/lu/btsi/bragi/ros/server/DatabaseModule.java
@@ -3,10 +3,13 @@ package lu.btsi.bragi.ros.server;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
@@ -21,9 +24,11 @@ public class DatabaseModule extends AbstractModule {
     @Singleton
     @Provides
     public DSLContext getConnection() {
-        String userName = "root";
-        String password = "";
-        String url = "jdbc:mysql://localhost:3306/ros?autoReconnect=true&useSSL=false";
+        Config config = ConfigFactory.parseFile(new File("ros.conf"));
+        String userName = config.getString("mysql.username");
+        String password = config.getString("mysql.password");
+        String db = config.getString("mysql.db");
+        String url = "jdbc:mysql://localhost:3306/" + db + "?autoReconnect=true&useSSL=false";
 
         try {
             Connection connection = DriverManager.getConnection(url, userName, password);
--- server/src/main/java/lu/btsi/bragi/ros/server/Main.java
+++ server/src/main/java/lu/btsi/bragi/ros/server/Main.java
@@ -1,12 +1,16 @@
 package lu.btsi.bragi.ros.server;
 
+import com.google.common.io.Files;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 import javax.jmdns.JmDNS;
 import javax.jmdns.ServiceInfo;
+import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 /**
  * Created by gillesbraun on 14/02/2017.
@@ -14,6 +18,16 @@ import java.net.InetAddress;
 public class Main {
 
     public static void main(String[] args) {
+        File configFile = new File("ros.conf");
+        if(!configFile.exists())
+            try {
+                URL reference = Main.class.getResource("/reference.conf");
+                Files.copy(new File(reference.toURI()), configFile);
+                System.err.println("Please fill out the configuration file that has been created: " + configFile.getAbsolutePath());
+                System.exit(0);
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
         Injector injector = Guice.createInjector(new DatabaseModule());
 
         Server main = injector.getInstance(Server.class);
